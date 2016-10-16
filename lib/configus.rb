794603cf5f5f825dbb4406b88836152fee08b297
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-17 01:24:26 +0400

    add field

--- lib/configus.rb
+++ lib/configus.rb
@@ -4,6 +4,7 @@ Configus.build Rails.env do
       karamzin do
         karamzin250 'http://karamzin250.ru'
         wikipedia 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%80%D0%B0%D0%BC%D0%B7%D0%B8%D0%BD,_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9_%D0%9C%D0%B8%D1%85%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2%D0%B8%D1%87'
+        github 'https://github.com/ulmic/karamzin'
       end
     end
   end
