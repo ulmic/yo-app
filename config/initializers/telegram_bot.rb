039c82c60f22db17cbdc681ed002ddbf1213f7af
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-17 18:50:07 +0400

    Edit messages

--- config/initializers/telegram_bot.rb
+++ config/initializers/telegram_bot.rb
@@ -8,9 +8,9 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Привет, #{message.from.first_name}")
     when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Пока, #{message.from.first_name}")
     else
       text = insert message.text
       increase_request_counter :telegram
