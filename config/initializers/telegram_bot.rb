2332a4755d11d3df4653b43c16e8a82cee26a27f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-17 18:32:02 +0400

    Abort initializer

--- config/initializers/telegram_bot.rb
+++ config/initializers/telegram_bot.rb
@@ -1,22 +1,22 @@
-if Rails.env.production?
-  require 'telegram/bot'
-  include ::Karamzin
-  include Concerns::RequestCounter
-
-  token = ENV['TELEGRAM_BOT_TOKEN']
-
-  Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
-      case message.text
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-      when '/stop'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-      else
-        text = insert message.text
-        increase_request_counter :telegram
-        bot.api.send_message(chat_id: message.chat.id, text: text)
-      end
-    end
-  end
-end
+#if Rails.env.production?
+#  require 'telegram/bot'
+#  include ::Karamzin
+#  include Concerns::RequestCounter
+#
+#  token = ENV['TELEGRAM_BOT_TOKEN']
+#
+#  Telegram::Bot::Client.run(token) do |bot|
+#    bot.listen do |message|
+#      case message.text
+#      when '/start'
+#        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+#      when '/stop'
+#        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+#      else
+#        text = insert message.text
+#        increase_request_counter :telegram
+#        bot.api.send_message(chat_id: message.chat.id, text: text)
+#      end
+#    end
+#  end
+#end
